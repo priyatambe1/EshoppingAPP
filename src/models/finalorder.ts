b9522f33697711cd539e71e7b535ece2c4a571e9
Tambe <priyanka-dilip.tambe@capgemini.com>
2023-12-28 11:33:36 +0530

    rr

---
+++ src/models/finalorder.ts
@@ -0,0 +1,15 @@
+export class finalorder
+{
+    id: number=0;
+    firstName: string='';
+    lastName: string='';
+    address: string='';
+    state: string='';
+    city: string='';
+    zip: number=0;
+    email: string='';
+    ProductName: string='';
+    ProductQuantity: number=0;
+    ProductPrice: number=0;
+    UserName: string='';
+  }
